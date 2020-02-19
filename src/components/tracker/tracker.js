97c4475893d474a3bde9f386376cfd0960ecaffc
Megan Henry <megangabriellehenry@gmail.com>
2020-02-19 13:51:42 -0600

    added extra function to only submit final number instead of adding as you type

--- src/components/tracker/tracker.js
+++ src/components/tracker/tracker.js
@@ -2,28 +2,43 @@ import React, {Component} from 'react';
 
 class Tracker extends Component {
     state = {
-        recentRun: 0,
-        longestRun: 0
+        inputObj: {
+            recent: 0,
+            longest: 0
+        },
+        finalObj: {
+            recent: 0,
+            longest: 0
+        }
     }
-  handleSubmit=(event)=>{
+  handleChange=(event)=>{
     console.log('in handleSubmit', event.target.value);
-    
     this.setState({
-      recentRun: event.target.value
+        inputObj: {
+            recent: event.target.value,
+        }
+      })
+  }
+  handleSubmit = () => {
+      this.setState({
+        finalObj: {
+            recent: this.state.inputObj.recent,
+            longest: this.state.inputObj.longest
+        }
       })
   }
   render () {
   return (
     <div>
         <label htmlFor="milesRan">Miles Ran: </label>
-        <input onChange={(event)=>this.handleSubmit(event)} placeholder="Miles Ran"/>
+        <input onChange={(event)=>this.handleChange(event)} placeholder="Miles Ran"/>
         <button onClick={this.handleSubmit}>Add New Run</button>
       <div>
           <p>
-            Recent Run: {this.state.recentRun}
+            Recent Run: {this.state.finalObj.recent}
           </p>
           <p>
-            Longest Run: {this.state.longestRun}
+            Longest Run: {this.state.finalObj.longest}
           </p>
       </div>
     </div>
